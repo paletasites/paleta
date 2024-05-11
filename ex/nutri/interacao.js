e163d05d397f0cec8398d758066e714233703414
pedroaugsuto14 <167724365+paletasites@users.noreply.github.com>
2024-05-11 17:36:10 -0300

    animacao

--- ex/nutri/interacao.js
+++ ex/nutri/interacao.js
@@ -1,30 +1,40 @@
+console.log("sinal de vida")
 
 // sessao do mais_um_pouco
 
-const posicao_tudo = 100 ;
 const contem_tudo = window.document.getElementById("tudo")
-const titulo_tudo = window.document.getElementById("titulo_tudo");
-const p_tudo = window.document.getElementById("p_tudo");
-const button_tudo = window.document.getElementById("button002");
+const titulo_tudo = window.document.getElementById("titulo_tudo")
+const p_tudo = window.document.getElementById("p_tudo")
+const button_tudo = window.document.getElementById("button002")
 const folha_tudo = window.document.getElementById("folha003")
 var tudo_afetado = false;
 
-
 window.addEventListener('scroll', function() {
-    var rolagem = window.scrollY;
-    console.log(rolagem);
+    var posicao_tudo = contem_tudo.getBoundingClientRect()
 
-    if (rolagem <= posicao_tudo && tudo_afetado == true){
-        console.log("vc n ve mais o tudo !!!")
-        contem_tudo.style.animation = "none"
-        tudo_afetado = false;
-    }
-
-    if (rolagem >= posicao_tudo && tudo_afetado == false){
-        console.log("vc chegou em tudo :)");
+    if (posicao_tudo.top < window.innerHeight - 200 && tudo_afetado == false) {
         contem_tudo.style.animation = "1s entrar_tudo"
         contem_tudo.style.animationFillMode = "forwards"
-        tudo_afetado = true;
-    }
 
+        titulo_tudo.style.animation = "1s entrar_lado_esquerdo"
+        titulo_tudo.style.animationFillMode = "forwards"
+
+        p_tudo.style.animation = "1s entrar_lado_direito"
+        p_tudo.style.animationFillMode = "forwards"
+
+        button_tudo.style.animation = "1s entrar_cima"
+        button_tudo.style.animationFillMode = "forwards"
+
+        folha_tudo.style.animation = "1s folha_rodando"
+        folha_tudo.style.animationFillMode = "forwards"
+
+        tudo_afetado = true;
+    }if (posicao_tudo.top > window.innerHeight - 200 && tudo_afetado == true) {
+        contem_tudo.style.animation = "none"
+        titulo_tudo.style.animation = "none"
+        p_tudo.style.animation = "none"
+        button_tudo.style.animation = "none"
+        folha_tudo.style.animation = "none"
+        tudo_afetado = false;
+    }
 });
